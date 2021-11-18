a650fe8f6e8317a21c5ecad54d3f012d59beb1da
Brandon <bcao4@stevens.edu>
2021-11-18 03:29:09 -0500

    added search functionality on top coin page. some card style changes

--- frontend/src/components/TopCoins/CoinInfo.js
+++ frontend/src/components/TopCoins/CoinInfo.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+//https://www.coingecko.com/en/api/documentation?
+// https://developers.coinranking.com/api/documentation
+// https://docs.cryptowat.ch/websocket-api
+//https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&developer_data=false
+
 const CoinInfo = () => {
   return (
     <div>
--- frontend/src/components/TopCoins/TopCoins.js
+++ frontend/src/components/TopCoins/TopCoins.js
@@ -6,13 +6,16 @@ import './TopCoins.css'
 
 
 // Followed pagination tutorial: https://www.youtube.com/watch?v=NZKUirTtxcg&t=1218s
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=100&page=1&sparkline=true&price_change_percentage=24h
 
-let baseUrl = "https://api.coingecko.com/api/v3/coins/markets"
+let baseUrl = "https://api.coingecko.com/api/v3/coins/markets";
+let coinListUrl = "https://api.coingecko.com/api/v3/coins/list";
 
 const TopCoins = () => {
   const [ loading, setLoading ] = useState(true);
   const [ pageNum, setPageNum ] = useState(1);
 	const [ coinData, setCoinData ] = useState([]);
+  const [ searchTerm, setSearchTerm ] = useState("");
 	const [ nextPageStatus, setNextPageStatus ] = useState(true);
 
   const observer = useRef();
@@ -33,6 +36,14 @@ const TopCoins = () => {
     console.log(node);
   }, [loading, nextPageStatus]);
 
+  const searchFunc = () => {
+    return coinData.filter((coin) => 
+      coin.id.toLowerCase().includes(searchTerm) ||
+      coin.symbol.toLowerCase().includes(searchTerm) ||
+      coin.name.toLowerCase().includes(searchTerm)
+    );
+  };
+
   useEffect(() => {
     setLoading(true);
     async function fetchData() {
@@ -42,9 +53,7 @@ const TopCoins = () => {
           url: baseUrl,
           params: {vs_currency: "usd", per_page: "40", sparkline: "true", price_change_percentage: "24h", page: pageNum}
         });
-        console.log(data);
         setCoinData((prevCoins) => [...prevCoins, ...data]);
-        //setCoinData(data);
         if(data.length < 40) {
           setNextPageStatus(false);
         } else {
@@ -69,23 +78,35 @@ const TopCoins = () => {
       <>
         <div className="container-fluid">
           <div className="row">
-            {coinData.map((coin, index) => {
-              let currentPrice = millify(coin.current_price);
-              let marketCap = millify(coin.market_cap);
-              let dayHigh = millify(coin.high_24h);
-              let dayLow = millify(coin.low_24h);
-              let priceChange = millify(coin.price_change_percentage_24h, {
-                precision:2
-              });
+            <form>
+              <input 
+                className="form-control" 
+                type="search" 
+                placeholder="Search for a Coin..." 
+                aria-label="Search"
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </form>
+            {searchFunc().map((coin, index) => {
+              let coinSymbol = coin.symbol ? coin.symbol : "No symbol";
+              let currentPrice = coin.current_price ? millify(coin.current_price) : "Not available";
+              let marketCap = coin.market_cap ? millify(coin.market_cap) : "Not available";
+              let dayHigh = coin.high_24h ? millify(coin.high_24h) : "Not available";
+              let dayLow = coin.low_24h ? millify(coin.low_24h) : "Not available";
+              let priceChange = coin.price_change_percentage_24h ? 
+                millify(coin.price_change_percentage_24h, { precision:2}) : "Not available";
               
-              if(coinData.length === index + 1) {
+              if(searchFunc().length === index + 1) {
                 return (
-                  <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-3" key={coin.name}>
+                  <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12" key={coin.name}>
                     <div className="card text-center" ref={lastCoinElementRef}>
                       <div className="card-header">
                           {index + 1}. {coin.name} <img src={coin.image} alt={coin.id} height="45" />
                       </div>
                       <div className="card-body">
+                        Symbol: {coinSymbol}
+                        <br/>
+                        <br/>
                         Current Price: ${currentPrice}   
                         <br/>
                         <br/>
@@ -109,11 +130,14 @@ const TopCoins = () => {
               } else {
                 return (
                   <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-3" key={coin.name}>
-                    <div className="card text-center border-dark h-100">
+                    <div className="card text-center border-0 h-100">
                       <div className="card-header h-100">
                         {index + 1}. {coin.name} <img id="top-coin-img" src={coin.image} alt={coin.id} height="45" />
                       </div>
                       <div className="card-body">
+                        Symbol: {coinSymbol}
+                        <br/>
+                        <br/>
                         Current Price: ${coin.current_price}   
                         <br/>
                         <br/>
