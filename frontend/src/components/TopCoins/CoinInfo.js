73fe6cffb9970666a4d49875631f9c9599f2e3b3
Brandon <bcao4@stevens.edu>
2021-12-02 01:55:07 -0500

    add single coin info page with chart

--- frontend/src/components/TopCoins/CoinInfo.js
+++ frontend/src/components/TopCoins/CoinInfo.js
@@ -1,17 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Line } from "react-chartjs-2";
+import ReactHtmlParser from 'react-html-parser';
+import TopCoins from './TopCoins';
+import './TopCoins.css'
 
-//https://www.coingecko.com/en/api/documentation?
+// https://www.coingecko.com/en/api/documentation?
 // https://developers.coinranking.com/api/documentation
 // https://docs.cryptowat.ch/websocket-api
-//https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&developer_data=false
+// https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&developer_data=false
+// https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1
 
 const CoinInfo = () => {
-  return (
-    <div>
-      CoinInfo
-    </div>
-  )
+  const [ loading, setLoading ] = useState(true);
+  const [ chartLoading, setChartLoading ] = useState(true);
+  const [ coinData, setCoinData ] = useState([]);
+  const [ chartData, setChartData ] = useState();
+  const navigate = useNavigate();
+  let params = useParams();
+  let coinID = params.id;
+
+  let coinInfoBaseUrl = "https://api.coingecko.com/api/v3/coins/";
+
+  useEffect(() => {
+    setLoading(true);
+    async function fetchData() {
+      try {
+        const { data } = await axios({
+          method: 'GET',
+          url: coinInfoBaseUrl + coinID,
+          params: {localization: "false", tickers: "false", developer_data: "false", sparkline: "true"}
+        });
+        setCoinData(data);
+        setLoading(false);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, [coinID]);
+
+  useEffect(() => {
+    setChartLoading(true);
+    async function fetchChartData() {
+      try {
+        let chartDataUrl = coinInfoBaseUrl + coinID + "/market_chart?vs_currency=usd&" + "days=7";
+        console.log(chartDataUrl);
+        const { data } = await axios.get(chartDataUrl);
+        setChartData(data.prices);
+        setChartLoading(false);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    fetchChartData();
+  }, [coinID]);
+
+  let coinDescription = coinData.description ? coinData.description.en.split(". ")[0] : "no description available";
+  let coinWebsite = coinData.links ? coinData.links.homepage[0] : "no website available";
+  let coinRank = coinData.market_cap_rank ? coinData.market_cap_rank : "no rank available";
+  let coinPrice = coinData.market_data ? coinData.market_data.current_price.usd : "no rank available";
+
+  if(loading || chartLoading) {
+    return (
+      <div className="white-text">
+        <h2>Loading....</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <br/>
+        <div className="white-text text-center">
+          <img src={coinData.image.small} alt={coinData.id} />
+          <h1 className="coin-name">
+            {coinData.id}
+          </h1>
+          <br/>
+          <br/>
+          <h3>
+            Market Cap Rank: {coinRank}
+          </h3>
+          <br/>
+          <h3>
+            Current Price: ${coinPrice}
+          </h3>
+          <br/>
+          <br/>
+          {ReactHtmlParser(coinDescription)}
+          <br/>
+          <br/>
+          Website: <a href={coinWebsite}> {coinWebsite} </a>
+          <br/>
+          <br/>
+          {/* Minutely data will be used for duration within 1 day, 
+          Hourly data will be used for duration between 1 day and 90 days, 
+          Daily data will be used for duration above 90 days. */}
+            <Line
+              data={{
+                labels: chartData.map((time) => {
+                  let hourlyData = new Date(time[0]).toLocaleString();
+                  return hourlyData;
+                }),
+                datasets: [
+                  {
+                    data: chartData.map((price) => price[1]),
+                    label: "Price in USD"
+                  }
+                ]
+              }}
+            />
+        </div>
+      </div>
+    )
+  }
 };
 
 export default CoinInfo;
