609286d5161029399f73f076eb0d734fd02d8460
omkarssinha <80606973+omkarssinha@users.noreply.github.com>
2021-12-04 01:51:28 -0500

    Added Homepage
    Added homepage with background pic and lighting.

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,5 +1,22 @@
+import React from "react";
+import './homePage.css';
+
+//backgroundImage:`url("${image}")`
+
 const Home = () => {
-  return <div>Home</div>;
+
+  return (
+    <div className="container">
+      <div className="text">
+        <br/>
+        <h1 className="firstHeader">Cryptocurrency Exchange</h1>
+        <br/>
+        <h2 className="secondHeader">
+        Explore the crptocurrency trends and trade coins and much more !!
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
