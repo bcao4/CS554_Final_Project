f02cf77302c7fe40ac06d8fe7d47541d567abf74
omkarssinha <80606973+omkarssinha@users.noreply.github.com>
2021-12-11 06:13:16 -0500

    update

--- frontend/src/components/BuySell/Trade.js
+++ frontend/src/components/BuySell/Trade.js
@@ -1,32 +1,78 @@
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import { useForm } from "react-hook-form";
 const GDAX = require("gdax");
-const publicClient = new GDAX.PublicClient();
 
-const getProductID = () => {
-
-    let list;
-    const callback = (error, response, data) => {
-    if (error)
-        return console.dir(error);
-    list =data
-    console.log(list)
-    //return console.dir(data);
-    return data;
-    }
-    publicClient.getProducts(callback);
-    console.log(list)
-    return list;
-
-}
 
 export default function Trade() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
 
-  let list = getProductID();
-  console.log(list)
-  //console.log(watch("example")); // watch input value by passing the name of it
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const publicClient = new GDAX.PublicClient();
+  const {productidState, setProductidState} = useState([]);
+  const apiURI = 'https://api.gdax.com';
+
+  const callback = (error, response, data) => {
+    if (error)
+        return console.dir(error);
+
+    return console.dir(data);;
+  }
+  
+  const onSubmit = data => 
+  {
+    console.log(data);
+
+    /* Authenticate user to GDAX(CoinBase Pro)*/
+    const authenticatedClient = new GDAX.AuthenticatedClient(data.gdaxKey, data.gdaxSecret, data.passPhrase, apiURI);
+
+    /* Buying */
+    function placeBuy(data) {
+
+        const buyParams = {
+    
+            'price': data.price,
+    
+            'size': data.size,
+    
+            'product_id': data.product_id,
+    
+        };
+        this.buyOrderId = authenticatedClient.buy(buyParams, callback);
+    
+    }
+
+    /* Selling */
+    function placeSell(data) {
+
+        const sellParams = {
+    
+            'price': data.price,
+    
+            'size': data.size,
+
+            'product_id': data.product_id,
+    
+        };
+        this.sellOrderId = authenticatedClient.sell(sellParams, callback); 
+    }
+
+    /* Check if user chose buy or sell */
+    if(data.buyOrSell == "buy")
+    placeBuy(data);
+    else
+    placeSell(data)
+
+  }
+  /* Get all Product-id like BTC_USD*/
+  /*const getProductID = () =>{
+    const callback = (error, response, data) => {
+      if (error)
+          return console.dir(error);
+  
+      return console.dir(data);;
+    }
+    publicClient.getProducts(callback);
+    }
+  */
 
   return (
     
@@ -47,15 +93,8 @@ export default function Trade() {
         <br/>
         <input defaultValue="Enter size" {...register("size", { required: true })} />
         <br/>
-        <select  {...register("product-id")}>
-            <option value="BTC_USD">BTC_USD</option>
-            {(getProductID()).map(i =>
-                <option value={i.id}>{i.id}</option>
-            )}
-        </select>
-
-
-
+        <input defaultValue="Enter Product_ID(BTC_USD)" {...register("product-id", { required: true })} />
+        <br/>
         {errors.exampleRequired && <span>Sorry! There was an error</span>}
         <br/>
         <input type="submit" />
