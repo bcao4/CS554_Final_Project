aecddebfefbe9c4728ab7260b6decf0b0a688b71
Michael Arrabito <mikearrabito@gmail.com>
2021-12-14 16:01:41 -0500

    yarn

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@ cd frontend
 rm -rf node_modules
 rm -rf package-lock.json
 npm i -g yarn
-yarn uninstall react-scripts
-yarn install -g react-scripts
+yarn global add react-scripts
 yarn install
 yarn run build
 cd ..
 mkdir backend/build
 mv frontend/build backend/
 cd backend
-yarn install --production
-yarn prune
+yarn install
