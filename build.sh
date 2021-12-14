f39f6e848a25b7c925386386759b440b24e5944d
Michael Arrabito <mikearrabito@gmail.com>
2021-12-14 15:58:25 -0500

    trying yarn

--- build.sh
+++ build.sh
@@ -2,14 +2,13 @@
 cd frontend
 rm -rf node_modules
 rm -rf package-lock.json
-npm uninstall react-scripts
-npm install -g react-scripts
-npm install
-npm install react@alpha react-dom@alpha
-npm run build
+yarn uninstall react-scripts
+yarn install -g react-scripts
+yarn install
+yarn run build
 cd ..
 mkdir backend/build
 mv frontend/build backend/
 cd backend
-npm install --production
-npm prune
+yarn install --production
+yarn prune
