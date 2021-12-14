4661b28daf780cfaddf5c7d773f5904696b79c6d
Michael Arrabito <mikearrabito@gmail.com>
2021-12-14 17:04:42 -0500

    docker setup

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 cd frontend
 rm -rf node_modules
 rm -rf package-lock.json
-npm i -g yarn
-yarn global add react-scripts install-peers
-yarn install install-peers
-yarn run build
+npm install -g react-scripts
+npm install
+npm run build
 cd ..
 mkdir backend/build
 mv frontend/build backend/
 cd backend
-yarn install
+npm install --production
