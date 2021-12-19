56c774e49ed10eba6b8d9d8d4257c832090f923e
Michael Arrabito <mikearrabito@gmail.com>
2021-12-19 18:32:17 -0500

    account page changes

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,19 +1,20 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import "./accountPage.css";
 import { AuthContext } from "../../firebase/Auth";
 import axios from "axios";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SignOutButton from "./SignOut";
-import { API_URL } from "../../api";
-import { getCoinInfo } from "../../api";
-//img
+import { API_URL, getCoinInfo } from "../../api";
+import { capitalize, convertPrice } from "../../utils";
 import UploadImage from "./UploadImage";
 import {
   Button,
   Dialog,
   DialogActions,
   DialogTitle,
+  ListItem,
   Typography,
+  List,
 } from "@mui/material";
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
@@ -24,6 +25,9 @@ const db = app.firestore();
 const Account = (props) => {
   const { currentUser } = useContext(AuthContext);
   const [userName, setUserName] = useState(undefined);
+  const interval = useRef(null);
+
+  const [symbols, setSymbols] = useState({});
 
   // Buying and selling code start ############################
   const [currBalance, setCurrBalance] = useState(0);
@@ -31,7 +35,7 @@ const Account = (props) => {
   const [accBalance, setAccBalance] = useState(0);
   const [loading, setLoading] = useState(true);
   const [defVal, setdefVal] = useState(0);
-// Buying and selling code end ##############################
+  // Buying and selling code end ##############################
 
   //img upload
   const [isImageDialogOpen, setImageDialog] = useState(false);
@@ -52,13 +56,13 @@ const Account = (props) => {
 
   useEffect(() => {
     console.log("render");
-    console.log("main useEffect fired")
+    console.log("main useEffect fired");
     async function fetchData() {
       try {
         let token = await currentUser.getIdToken();
         let config = {
           method: "get",
-          url: `${API_URL}/users/` + currentUser.email, //removedhttp for API call
+          url: `${API_URL}/users/${currentUser.email}`,
           headers: {
             accept: "application/json",
             "Accept-Language": "en-US,en;q=0.8",
@@ -75,6 +79,7 @@ const Account = (props) => {
         for (let i of data.coins) {
           if (Object.values(i)[0] > 0) coinListDisplay.push(i);
         }
+        console.log(data);
         setCurrCoins(coinListDisplay);
         // Buying and selling code end ############################
       } catch (e) {
@@ -105,86 +110,89 @@ const Account = (props) => {
 
   // Buying and selling code start ############################
   // useEffect with setInterval 10 sec to update price
-  useEffect(async() => {
-    console.log("buying useEffect fired")
-
-      const fetchData = async (coin, num) => {
-        try {
-          setLoading(true);
-          const [coinData] = await Promise.all([
-            getCoinInfo(coin),
-          ]);
-          console.log(coin + " " +num +" "+ coinData.market_data.current_price.usd)
-          console.log(coinData.market_data.current_price.usd * num)
-          //setAccBalance(parseFloat(accBalance) + (coinData.market_data.current_price.usd * num))
-          //console.log(accBalance)
-          return (coinData.market_data.current_price.usd * num)
-  
-        } catch (e) {
-          console.error(e);
-        } finally {
-          setLoading(false);
-        }
-      };
-
-    const interval = setInterval(async function () {
-      
-      let val =0;
-      for(let i of currCoins)
-      {
-        val =val + await fetchData(Object.keys(i)[0], Object.values(i)[0]);
-      }
-      //console.log(count);
-      setAccBalance(val)
-    }, 7000);
-
-    return async() => {
-      console.log("cleanup");
-      clearInterval(interval);
-  }
-  }, [currCoins]);
-
-  // useEffect without setInterval to fire when accBalane changes to prevent delay
-  useEffect(async() => {
+  useEffect(async () => {
+    console.log("buying useEffect fired");
 
     const fetchData = async (coin, num) => {
       try {
         setLoading(true);
-        const [coinData] = await Promise.all([
-          getCoinInfo(coin),
-        ]);
-        console.log(coin + " " +num +" "+ coinData.market_data.current_price.usd)
-        console.log(coinData.market_data.current_price.usd * num)
+        const [coinData] = await Promise.all([getCoinInfo(coin)]);
+        console.log(
+          coin + " " + num + " " + coinData.market_data.current_price.usd
+        );
+        console.log(coinData.market_data.current_price.usd * num);
         //setAccBalance(parseFloat(accBalance) + (coinData.market_data.current_price.usd * num))
         //console.log(accBalance)
-        return (coinData.market_data.current_price.usd * num)
-
+        return coinData.market_data.current_price.usd * num;
       } catch (e) {
         console.error(e);
       } finally {
         setLoading(false);
       }
     };
-    let defVal=0;
-        for(let i of currCoins)
-        {
-          defVal =defVal + await fetchData(Object.keys(i)[0], Object.values(i)[0]);
-        }
-      if(!accBalance)
-        setdefVal((parseFloat(defVal) + parseFloat(currBalance)).toFixed(2))
 
-}, [accBalance]);
+    const updateAccVal = async () => {
+      let val = 0;
+      for (let i of currCoins) {
+        val = val + (await fetchData(Object.keys(i)[0], Object.values(i)[0]));
+      }
+      //console.log(count);
+      setAccBalance(val);
+    };
+
+    updateAccVal();
+    interval.current = setInterval(updateAccVal, 7000);
+
+    return () => {
+      console.log("cleanup");
+      clearInterval(interval.current);
+    };
+  }, [currCoins]);
+
+  // useEffect without setInterval to fire when accBalane changes to prevent delay
+  useEffect(async () => {
+    const fetchData = async (coin, num) => {
+      try {
+        setLoading(true);
+        const [coinData] = await Promise.all([getCoinInfo(coin)]);
+        console.log(
+          coin + " " + num + " " + coinData.market_data.current_price.usd
+        );
+        console.log(coinData.market_data.current_price.usd * num);
+        setSymbols((old) => {
+          return {
+            ...old,
+            [coinData.id]: `${coinData.symbol.toUpperCase()} - $${convertPrice(
+              coinData.market_data.current_price.usd * num
+            )}`,
+          };
+        });
+        //setAccBalance(parseFloat(accBalance) + (coinData.market_data.current_price.usd * num))
+        //console.log(accBalance)
+        return coinData.market_data.current_price.usd * num;
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    let defVal = 0;
+    for (let i of currCoins) {
+      defVal =
+        defVal + (await fetchData(Object.keys(i)[0], Object.values(i)[0]));
+    }
+    if (!accBalance)
+      setdefVal((parseFloat(defVal) + parseFloat(currBalance)).toFixed(2));
+  }, [accBalance]);
 
   // Buying and selling code end ##############################
 
   const changePassword = () => {
     if (currentUser.providerData[0].providerId === "password") {
       return (
-        <button className="btn-reset-pswd">
-          <NavLink exact to="/changepassword" activeclassname="active">
-            Reset password
-          </NavLink>
-        </button>
+        <Button variant="contained" component={Link} to="/changepassword">
+          Reset password
+        </Button>
       );
     } else {
       return null;
@@ -199,54 +207,65 @@ const Account = (props) => {
             Account Details
           </Typography>
           <div>
-            <p>
-              <img
-                width={150}
-                height={150}
-                src={
-                  userData.imageUrl
-                    ? userData.imageUrl
-                    : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
-                }
-                alt="Profile icon"
-              />
-            </p>
-            <b>Name: </b>
-            {userName}
-            <br />
-            <br />
-            <b>Email: </b>
-            {currentUser.email}
+            <img
+              className="profile-pic"
+              src={
+                userData.imageUrl
+                  ? userData.imageUrl
+                  : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
+              }
+              alt="Profile icon"
+            />
+            <Typography fontWeight={600} style={{ marginTop: "10px" }}>
+              Name
+            </Typography>
+            <Typography>{userName}</Typography>
+            <Typography fontWeight={600} style={{ marginTop: "10px" }}>
+              Email
+            </Typography>
+            <Typography>{currentUser.email}</Typography>
           </div>
           {/*// Buying and selling code start ############################*/}
           <div>
-          <br />
-          <b>Cash Balance: </b>
-          ${parseFloat(currBalance).toFixed(2)}
-          <br />
-          <br />
-          <b>Current coins: </b>
-          {currCoins.length?            
-            (currCoins).map(i =>( 
-          <li> 
-          <NavLink exact to={`/coin/${Object.keys(i)[0]}`} >{Object.keys(i)[0]} </NavLink>: {Object.values(i)[0]} 
-          </li>
-          )): "None" }
-          <br />
-          <b>Total Account value: $ </b>
-          {parseInt(accBalance)?
-          (parseFloat(accBalance) + parseFloat(currBalance)).toFixed(2):
-          (parseInt(defVal)? defVal: "loading...")
-          }
+            <Typography fontWeight={600} style={{ marginTop: "10px" }}>
+              Cash Balance
+            </Typography>
+            ${convertPrice(currBalance)}
+            <Typography fontWeight={600} style={{ marginTop: "10px" }}>
+              Current Coins
+            </Typography>
+            <List>
+              {currCoins.length
+                ? currCoins.map((i) => (
+                    <ListItem key={Object.keys(i)[0]}>
+                      <Link to={`/coin/${Object.keys(i)[0]}`}>
+                        {capitalize(Object.keys(i)[0])}
+                      </Link>
+                      : {Object.values(i)[0]} {symbols[Object.keys(i)[0]]}
+                    </ListItem>
+                  ))
+                : "None"}
+            </List>
+            <b>Total Account value: $ </b>
+            {parseInt(accBalance)
+              ? (parseFloat(accBalance) + parseFloat(currBalance)).toFixed(2)
+              : parseInt(defVal)
+              ? defVal
+              : "loading..."}
           </div>
           {/*// Buying and selling code end ############################*/}
-          <br />
           {/* Image Upload Button */}
-          <button className="btn-image-upload" onClick={openImageDialog}>
-            Upload Image
-          </button>
-          {changePassword()}
-          <SignOutButton />
+          <div style={{ marginTop: "10px" }}>
+            <Button
+              variant="contained"
+              onClick={openImageDialog}
+              style={{ marginRight: "6px" }}
+            >
+              Upload Image
+            </Button>
+            {changePassword()}
+            <SignOutButton style={{ marginLeft: "6px" }} />
+          </div>
 
           {/* Image dialog box */}
           <Dialog
@@ -254,22 +273,28 @@ const Account = (props) => {
             onClose={handleClickCancel}
             PaperProps={{
               style: {
-                height: 350,
-                width: 500,
+                height: 500,
+                width: 600,
               },
             }}
           >
-            <DialogTitle>
-              Upload Profile Picture
-              <IconButton onClick={handleClickCancel} className="close-icon">
-                <CloseIcon />
-              </IconButton>
-            </DialogTitle>
-            <UploadImage />
-            <DialogActions>
-              <Button onClick={handleClickCancel}>Cancel</Button>
-              <Button onClick={handleClickOk}>Ok</Button>
-            </DialogActions>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <DialogTitle>
+                Upload Profile Picture
+                <IconButton onClick={handleClickCancel} className="close-icon">
+                  <CloseIcon />
+                </IconButton>
+              </DialogTitle>
+              <UploadImage />
+              <DialogActions>
+                <Button variant="contained" onClick={handleClickCancel}>
+                  Cancel
+                </Button>
+                <Button variant="contained" onClick={handleClickOk}>
+                  Ok
+                </Button>
+              </DialogActions>
+            </div>
           </Dialog>
         </div>
       </div>
